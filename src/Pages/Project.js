c052c0042a2b6887d26019accfe3f96e329c76b5
Kinuganjhu <kinuganjhu02@gmail.com>
2024-06-28 13:43:42 +0530

    add modal

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -1,19 +1,55 @@
-import Data from './Data'
-import {Button} from 'react-bootstrap';
-const Project =()=>{
-  return(
+import React, { useState } from 'react';
+import Data from './Data'; 
+import { Button, Modal } from 'react-bootstrap';
+
+
+const Project = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
     <>
-      <div class='container'> 
-    <h1>My Portofolio</h1> 
-    {Data.map((item)=>(
-  <div key ={item.id} className='item'>
-  <img src ={item.logo}/>
-    <h6>{item.title}</h6>
-    
-  </div>
-    ))}
+      <div className='container'>
+        <h1>My Portfolio</h1>
+        {Data.map((item) => (
+          <div key={item.id} className='item'>
+            <img src={item.logo} alt={item.title} />
+            <h6>{item.title}</h6>
+            <Button variant='link' onClick={() => openModal(item)}>View</Button>
+          </div>
+        ))}
       </div>
+
+      {/* Modal */}
+      <Modal show={showModal} onHide={closeModal}
+      className='info'
+     placement='bottom'
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedItem ? selectedItem.title : ''}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className ='info'>
+          {selectedItem ? selectedItem.description : ''}
+          <br />
+          <a href={selectedItem ? selectedItem.Github : '#'}>GitHub &rarr;</a>
+          <br />
+          <a href={selectedItem ? selectedItem.url : '#'}>Live Preview &rarr;</a>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={closeModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </>
-    )
-}
+  );
+};
+
 export default Project;
