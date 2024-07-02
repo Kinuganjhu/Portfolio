69cad9c04c3780e8799f327a1888f94a18192d1a
Frontend Developer <136769553+Kinuganjhu@users.noreply.github.com>
2024-07-02 07:15:21 +0530

    Update Resume

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -15,7 +15,7 @@ const Home =()=>{
       <h1>Junior Frontend Developer
       </h1>
       <br/>
-<a href="https://drive.google.com/file/d/1KAsSx7KjdYEZ8Ns21nHkGvDvOefy-M1_/view" target="_blank" rel="noopener noreferrer">
+<a href="https://drive.google.com/file/d/1HaKIKOYM775JxvRsIcbOji3aGBX6aiiJ/view?usp=drivesdk" target="_blank" rel="noopener noreferrer">
   <Button variant="light" className="Btn">
     Download CV
   </Button>
