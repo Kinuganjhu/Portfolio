f847e28237a988e378008b982a08a9f8fe6dfba0
Kinuganjhu <kinuganjhu02@gmail.com>
2024-08-09 08:31:46 +0530

    change project style

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
 import Data from './Data'; 
-import { Button, Modal } from 'react-bootstrap';
-
+import { Button, Offcanvas } from 'react-bootstrap';
 
 const Project = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
 
-  const openModal = (item) => {
+  const openOffcanvas = (item) => {
     setSelectedItem(item);
-    setShowModal(true);
+    setShowOffcanvas(true);
   };
 
-  const closeModal = () => {
-    setShowModal(false);
+  const closeOffcanvas = () => {
+    setShowOffcanvas(false);
   };
 
   return (
@@ -23,36 +22,34 @@ const Project = () => {
         {Data.map((item) => (
           <div key={item.id} className='item'>
             <img src={item.logo} alt={item.title} />
-          <div className='Details'>
-  <b>{item.title}</b>
-            <Button 
-            variant='link'
-            onClick={() => openModal(item)}
-           >View</Button>
-           </div>
+            <div className='Details'>
+              <b>{item.title}</b>
+              <Button 
+                variant='link'
+                onClick={() => openOffcanvas(item)}
+              >
+                View
+              </Button>
+            </div>
           </div>
         ))}
       </div>
 
-      {/* Modal */}
-      <Modal show={showModal} onHide={closeModal}
-      className='info'
-     
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>{selectedItem ? selectedItem.title : ''}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className ='info'>
+      {/* Offcanvas */}
+      <Offcanvas show={showOffcanvas} onHide={closeOffcanvas} className='info' placement='bottom'>
+        <Offcanvas.Header
+        className='info'
+        >
+          <Offcanvas.Title>{selectedItem ? selectedItem.title : ''}</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body className='info'>
           {selectedItem ? selectedItem.description : ''}
           <br />
           <a href={selectedItem ? selectedItem.Github : '#'}>GitHub &rarr;</a>
           <br />
           <a href={selectedItem ? selectedItem.url : '#'}>Live Preview &rarr;</a>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={closeModal}>Close</Button>
-        </Modal.Footer>
-      </Modal>
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 };
